a5d62ea4ee6032494ef753a2bac6f4e4161c13fe
FinFET <76776190+FinFetChannel@users.noreply.github.com>
2022-01-26 13:36:38 -0300

    added continous fire

--- 8 - FPS mod.py
+++ 8 - FPS mod.py
@@ -80,16 +80,17 @@ def main():
             splash_screen(msg, surf, clock, font, screen)
             pause, clickdelay = 1, ticks
             pg.time.wait(500)
-        
+
+        if pg.mouse.get_pressed()[0]:
+            if swordsp < 1 and not pause:
+                swordsp, damage_mod = 1, 1
+            if pause and ticks - clickdelay > 1:
+                click, clickdelay = 1, ticks
+                sounds['healthup'].play()
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
-            if event.type == pg.MOUSEBUTTONDOWN:
-                if swordsp < 1 and not pause:
-                    swordsp, damage_mod = 1, 1
-                if pause and ticks - clickdelay > 1:
-                    click, clickdelay = 1, ticks
-                    sounds['healthup'].play()
                     
             if event.type == pg.KEYDOWN:
                 if event.key == ord('p') or event.key == pg.K_ESCAPE:
